8d25faf70e855d63f0cbd4de07a31130b21c9d3c
Harika <lakshmimaha683@gmail.com>
2025-06-16 09:32:17 +0530

    updated frontend config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 4173,
-    allowedHosts: ['resume-consultant-crwc.onrender.com/']
+    allowedHosts: ['resume-consultant-crwc.onrender.com']
   }
 })
